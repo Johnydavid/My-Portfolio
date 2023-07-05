89dd6f796d447ae5649a461b4306f977cb301abf
Johnydavid <dropin2johny@gmail.com>
2023-07-05 11:37:25 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-
 import React from "react";
-
 import Home from "./Home";
 import Contact from "./Contact";
-
 import About from "./About";
-
 import Experience from "./Experience";
 import Projects from "./Projects";
+import Certifcates from "./Certificates";
 
 
 export default function App() {
@@ -19,6 +16,7 @@ export default function App() {
 <About /> 
 <Experience />
 <Projects />
+<Certifcates />
 <Contact /> 
 
 
