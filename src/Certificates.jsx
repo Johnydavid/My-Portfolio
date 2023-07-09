3f47c1900faa74e3ad61ef99489ebf1e26b05476
Johnydavid <dropin2johny@gmail.com>
2023-07-09 11:21:47 +0530

    c

--- src/Certificates.jsx
+++ src/Certificates.jsx
@@ -71,16 +71,66 @@ const images = [
     target: "_blank",
     rel: "noreferrer",
   },
+  {
+    src: "https://media.licdn.com/dms/image/D5622AQEAlWq6Joynuw/feedshare-shrink_1280/0/1688880620266?e=1691625600&v=beta&t=VXwhMT0K7Nea2DEzRWUzbLs7BQ20qOlbNn2ymgrey7w",
+    width: 320,
+    height: 174, 
+    alt: "JavaScript - Guvi Certificate",
+    href: "https://media.licdn.com/dms/image/D5622AQEAlWq6Joynuw/feedshare-shrink_1280/0/1688880620266?e=1691625600&v=beta&t=VXwhMT0K7Nea2DEzRWUzbLs7BQ20qOlbNn2ymgrey7w",
+    target: "_blank",
+    rel: "noreferrer",
+  },
+  {
+    src: "https://media.licdn.com/dms/image/D5622AQFgu4fURaIghg/feedshare-shrink_1280/0/1688880667576?e=1691625600&v=beta&t=qs1V9zZ-j5tIcxDJBOSVwND7ofnIoQWX89enjecvCwc",
+    width: 320,
+    height: 174, 
+    alt: "MongoDB - Guvi Certificate",
+    href: "https://media.licdn.com/dms/image/D5622AQFgu4fURaIghg/feedshare-shrink_1280/0/1688880667576?e=1691625600&v=beta&t=qs1V9zZ-j5tIcxDJBOSVwND7ofnIoQWX89enjecvCwc",
+    target: "_blank",
+    rel: "noreferrer",
+  },
 
-//   {
-//     src: "https://st3.depositphotos.com/4187167/14640/i/1600/depositphotos_146407923-stock-photo-makeup-products-in-shopping-cart.jpg",
-//     width: 320,
-//     height: 212,
-//     alt: "Shopping Cart",
-//     href: "https://visitshopping-cart.netlify.app/",
-//     target: "_blank",
-//     rel: "noreferrer",
-//   },
+  {
+    src: "https://media.licdn.com/dms/image/D5622AQHvxzKbfFRhIQ/feedshare-shrink_1280/0/1688880684020?e=1691625600&v=beta&t=W0j4MCqRI26BteLULe5VcrATBBQRq_J7L0fE3SAsiaM",
+    width: 320,
+    height: 174, 
+    alt: "DS - Guvi Certificate",
+    href: "https://media.licdn.com/dms/image/D5622AQHvxzKbfFRhIQ/feedshare-shrink_1280/0/1688880684020?e=1691625600&v=beta&t=W0j4MCqRI26BteLULe5VcrATBBQRq_J7L0fE3SAsiaM",
+    target: "_blank",
+    rel: "noreferrer",
+  },
+
+  {
+    src: "https://media.licdn.com/dms/image/D5622AQEA4JfJTzyPNQ/feedshare-shrink_1280/0/1688880697694?e=1691625600&v=beta&t=H52fNrC3fgBoJKwj7BUBTOgE15czL-4uN9TPy-OAYnI",
+    width: 320,
+    height: 174, 
+    alt: "AWS - Guvi Certificate",
+    href: "https://media.licdn.com/dms/image/D5622AQEA4JfJTzyPNQ/feedshare-shrink_1280/0/1688880697694?e=1691625600&v=beta&t=H52fNrC3fgBoJKwj7BUBTOgE15czL-4uN9TPy-OAYnI",
+    target: "_blank",
+    rel: "noreferrer",
+  },
+
+  {
+    src: "https://media.licdn.com/dms/image/D5622AQEyZdvqLnQbsQ/feedshare-shrink_1280/0/1688880735058?e=1691625600&v=beta&t=Ttw9pD51XxspmS8d_fcN88Q0kOMt-t4c8XpBioJrLWY",
+    width: 320,
+    height: 174, 
+    alt: "ReactJS - Guvi Certificate",
+    href: "https://media.licdn.com/dms/image/D5622AQEyZdvqLnQbsQ/feedshare-shrink_1280/0/1688880735058?e=1691625600&v=beta&t=Ttw9pD51XxspmS8d_fcN88Q0kOMt-t4c8XpBioJrLWY",
+    target: "_blank",
+    rel: "noreferrer",
+  },
+
+  {
+    src: "https://media.licdn.com/dms/image/D5622AQG85KahVUDnMA/feedshare-shrink_1280/0/1688880745292?e=1691625600&v=beta&t=VLPG65hWyaCFJAMhcl0ug-EWxOmXekUCgRP2WrF0Wso",
+    width: 320,
+    height: 174, 
+    alt: "NodeJS - Guvi Certificate",
+    href: "https://media.licdn.com/dms/image/D5622AQG85KahVUDnMA/feedshare-shrink_1280/0/1688880745292?e=1691625600&v=beta&t=VLPG65hWyaCFJAMhcl0ug-EWxOmXekUCgRP2WrF0Wso",
+    target: "_blank",
+    rel: "noreferrer",
+  },
+
+
 ];
 
 export default function Certificates() {
