073b12a7afbedb4624603f32318694133a856e91
Johnydavid <dropin2johny@gmail.com>
2023-08-10 20:57:53 +0530

    c

--- src/components/Certificates/Certificates_Images.js
+++ src/components/Certificates/Certificates_Images.js
@@ -1,92 +1,90 @@
 export const images = [
   {
     original:
-      "https://media.licdn.com/dms/image/D5622AQEmZ6UjAsXCxg/feedshare-shrink_800/0/1688534480062?e=1691625600&v=beta&t=gPXUlmLuLbKG5Iu4HZo7HAyNDso6xhTqEkPfHrQW5wU",
+      "https://drive.google.com/uc?export=view&id=1-XWrHKKp7Emg7UWdRwVRWMZJ_hpQDbe3",
+
     thumbnail:
-      "https://media.licdn.com/dms/image/D5622AQEmZ6UjAsXCxg/feedshare-shrink_800/0/1688534480062?e=1691625600&v=beta&t=gPXUlmLuLbKG5Iu4HZo7HAyNDso6xhTqEkPfHrQW5wU",
+      "https://drive.google.com/uc?export=view&id=1-XWrHKKp7Emg7UWdRwVRWMZJ_hpQDbe3",
     alt: "FSD",
   },
 
   {
     original:
-      "https://media.licdn.com/dms/image/D5622AQF9-lA1gqALow/feedshare-shrink_1280/0/1688880568603?e=1691625600&v=beta&t=78hXpcw939AZ7Nm1ol_AnS04VmSRjs2rVYvYxIbvxUk",
+      "https://drive.google.com/uc?export=view&id=1-SvQS5cseUeYS7ZV7GLsLfOs3hhoQehv",
+
     thumbnail:
-      "https://media.licdn.com/dms/image/D5622AQF9-lA1gqALow/feedshare-shrink_1280/0/1688880568603?e=1691625600&v=beta&t=78hXpcw939AZ7Nm1ol_AnS04VmSRjs2rVYvYxIbvxUk",
+      "https://drive.google.com/uc?export=view&id=1-SvQS5cseUeYS7ZV7GLsLfOs3hhoQehv",
+
     alt: "Module 1 - HTML5",
   },
 
   {
     original:
-      "https://media.licdn.com/dms/image/D5622AQFLE0LRXFMOvA/feedshare-shrink_1280/0/1688880588219?e=1691625600&v=beta&t=FwnDNqUga1enQnHxdO32rM5wPZ2sTkKjumCG31gegmY",
+      "https://drive.google.com/uc?export=view&id=1-Z6MfIvuioQGkKCzlH0QPDtfAKDkJNmj",
     thumbnail:
-      "https://media.licdn.com/dms/image/D5622AQFLE0LRXFMOvA/feedshare-shrink_1280/0/1688880588219?e=1691625600&v=beta&t=FwnDNqUga1enQnHxdO32rM5wPZ2sTkKjumCG31gegmY",
+      "https://drive.google.com/uc?export=view&id=1-Z6MfIvuioQGkKCzlH0QPDtfAKDkJNmj",
     alt: "Module 2 - CSS3",
   },
 
   {
-    src: "https://media.licdn.com/dms/image/D5622AQGFvc4ViZK4ZA/feedshare-shrink_1280/0/1688880603827?e=1691625600&v=beta&t=hVKbQYotA358PrHpRGXi7p0Pgt8vsq6J0ED42EC0R6s",
     original:
-      "https://media.licdn.com/dms/image/D5622AQGFvc4ViZK4ZA/feedshare-shrink_1280/0/1688880603827?e=1691625600&v=beta&t=hVKbQYotA358PrHpRGXi7p0Pgt8vsq6J0ED42EC0R6s",
+      "https://drive.google.com/uc?export=view&id=1-aDi34EYYdzY7QDiZGoi_vgYsgL_sYpP",
     thumbnail:
-      "https://media.licdn.com/dms/image/D5622AQGFvc4ViZK4ZA/feedshare-shrink_1280/0/1688880603827?e=1691625600&v=beta&t=hVKbQYotA358PrHpRGXi7p0Pgt8vsq6J0ED42EC0R6s",
+      "https://drive.google.com/uc?export=view&id=1-aDi34EYYdzY7QDiZGoi_vgYsgL_sYpP",
     alt: "Module 3 - Bootstrap",
   },
   {
-    src: "https://media.licdn.com/dms/image/D5622AQEAlWq6Joynuw/feedshare-shrink_1280/0/1688880620266?e=1691625600&v=beta&t=VXwhMT0K7Nea2DEzRWUzbLs7BQ20qOlbNn2ymgrey7w",
     original:
-      "https://media.licdn.com/dms/image/D5622AQEAlWq6Joynuw/feedshare-shrink_1280/0/1688880620266?e=1691625600&v=beta&t=VXwhMT0K7Nea2DEzRWUzbLs7BQ20qOlbNn2ymgrey7w",
+      "https://drive.google.com/uc?export=view&id=1-PK1QmjZNMYj_yBIqoIxoMK0aERqZoYc",
     thumbnail:
-      "https://media.licdn.com/dms/image/D5622AQEAlWq6Joynuw/feedshare-shrink_1280/0/1688880620266?e=1691625600&v=beta&t=VXwhMT0K7Nea2DEzRWUzbLs7BQ20qOlbNn2ymgrey7w",
+      "https://drive.google.com/uc?export=view&id=1-PK1QmjZNMYj_yBIqoIxoMK0aERqZoYc",
     alt: "Module 4 - Javascript",
   },
   {
     original:
-      "https://media.licdn.com/dms/image/D5622AQHvxzKbfFRhIQ/feedshare-shrink_1280/0/1688880684020?e=1691625600&v=beta&t=W0j4MCqRI26BteLULe5VcrATBBQRq_J7L0fE3SAsiaM",
+      "https://drive.google.com/uc?export=view&id=1-Z0cxRNO6KCMQhNsFwlRIJEAYiHUThrn",
     thumbnail:
-      "https://media.licdn.com/dms/image/D5622AQHvxzKbfFRhIQ/feedshare-shrink_1280/0/1688880684020?e=1691625600&v=beta&t=W0j4MCqRI26BteLULe5VcrATBBQRq_J7L0fE3SAsiaM",
+      "https://drive.google.com/uc?export=view&id=1-Z0cxRNO6KCMQhNsFwlRIJEAYiHUThrn",
     alt: "Module 5 - DS",
   },
 
   {
-    src: "https://media.licdn.com/dms/image/D5622AQEyZdvqLnQbsQ/feedshare-shrink_1280/0/1688880735058?e=1691625600&v=beta&t=Ttw9pD51XxspmS8d_fcN88Q0kOMt-t4c8XpBioJrLWY",
     original:
-      "https://media.licdn.com/dms/image/D5622AQEyZdvqLnQbsQ/feedshare-shrink_1280/0/1688880735058?e=1691625600&v=beta&t=Ttw9pD51XxspmS8d_fcN88Q0kOMt-t4c8XpBioJrLWY",
+      "https://drive.google.com/uc?export=view&id=1-7PQit10cizoWZjm2ofH8USKwEJ5svET",
     thumbnail:
-      "https://media.licdn.com/dms/image/D5622AQEyZdvqLnQbsQ/feedshare-shrink_1280/0/1688880735058?e=1691625600&v=beta&t=Ttw9pD51XxspmS8d_fcN88Q0kOMt-t4c8XpBioJrLWY",
+      "https://drive.google.com/uc?export=view&id=1-7PQit10cizoWZjm2ofH8USKwEJ5svET",
     alt: "Module 6 - React JS",
   },
 
   {
     original:
-      "https://media.licdn.com/dms/image/D5622AQHVENHkHier-A/feedshare-shrink_1280/0/1688880754310?e=1691625600&v=beta&t=HBAE9snTQdrLtdtHwxCsdCLTTR8x_p76yh4mdBVXfzg",
+      "https://drive.google.com/uc?export=view&id=1-GSTzOjKq1rQIJleHt1T75CUs1TSeilR",
     thumbnail:
-      "https://media.licdn.com/dms/image/D5622AQHVENHkHier-A/feedshare-shrink_1280/0/1688880754310?e=1691625600&v=beta&t=HBAE9snTQdrLtdtHwxCsdCLTTR8x_p76yh4mdBVXfzg",
+      "https://drive.google.com/uc?export=view&id=1-GSTzOjKq1rQIJleHt1T75CUs1TSeilR",
     alt: "Module 7 - MySQL",
   },
 
   {
-    src: "https://media.licdn.com/dms/image/D5622AQFgu4fURaIghg/feedshare-shrink_1280/0/1688880667576?e=1691625600&v=beta&t=qs1V9zZ-j5tIcxDJBOSVwND7ofnIoQWX89enjecvCwc",
     original:
-      "https://media.licdn.com/dms/image/D5622AQFgu4fURaIghg/feedshare-shrink_1280/0/1688880667576?e=1691625600&v=beta&t=qs1V9zZ-j5tIcxDJBOSVwND7ofnIoQWX89enjecvCwc",
+      "https://drive.google.com/uc?export=view&id=1-NroKm4MXK3c7d8-eRhofGcJ0Kb31SFo",
     thumbnail:
-      "https://media.licdn.com/dms/image/D5622AQFgu4fURaIghg/feedshare-shrink_1280/0/1688880667576?e=1691625600&v=beta&t=qs1V9zZ-j5tIcxDJBOSVwND7ofnIoQWX89enjecvCwc",
+      "https://drive.google.com/uc?export=view&id=1-NroKm4MXK3c7d8-eRhofGcJ0Kb31SFo",
     alt: "Module 8 - MongoDB",
   },
 
   {
-    src: "https://media.licdn.com/dms/image/D5622AQG85KahVUDnMA/feedshare-shrink_1280/0/1688880745292?e=1691625600&v=beta&t=VLPG65hWyaCFJAMhcl0ug-EWxOmXekUCgRP2WrF0Wso",
     original:
-      "https://media.licdn.com/dms/image/D5622AQG85KahVUDnMA/feedshare-shrink_1280/0/1688880745292?e=1691625600&v=beta&t=VLPG65hWyaCFJAMhcl0ug-EWxOmXekUCgRP2WrF0Wso",
+      "https://drive.google.com/uc?export=view&id=1-E7suGfOlOC2IzvPgkMfsvMyLIAI0bAp",
     thumbnail:
-      "https://media.licdn.com/dms/image/D5622AQG85KahVUDnMA/feedshare-shrink_1280/0/1688880745292?e=1691625600&v=beta&t=VLPG65hWyaCFJAMhcl0ug-EWxOmXekUCgRP2WrF0Wso",
+      "https://drive.google.com/uc?export=view&id=1-E7suGfOlOC2IzvPgkMfsvMyLIAI0bAp",
     alt: "Module 9 - Node JS",
   },
 
   {
     original:
-      "https://media.licdn.com/dms/image/D5622AQEA4JfJTzyPNQ/feedshare-shrink_1280/0/1688880697694?e=1691625600&v=beta&t=H52fNrC3fgBoJKwj7BUBTOgE15czL-4uN9TPy-OAYnI",
+      "https://drive.google.com/uc?export=view&id=1-3sGXnfDg3FKAZ94CEov1Won3c4NMdNy",
     thumbnail:
-      "https://media.licdn.com/dms/image/D5622AQEA4JfJTzyPNQ/feedshare-shrink_1280/0/1688880697694?e=1691625600&v=beta&t=H52fNrC3fgBoJKwj7BUBTOgE15czL-4uN9TPy-OAYnI",
+      "https://drive.google.com/uc?export=view&id=1-3sGXnfDg3FKAZ94CEov1Won3c4NMdNy",
     alt: "Module 10 - AWS",
   },
 ];
